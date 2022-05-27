43b6c59edc862814ca14b8a2009f4d3776da9b54
pozarnik <pozarnik@tut.by>
2022-05-27 03:26:39 +0300

    Delete list_display_links from tags admin

--- backend/tags/admin.py
+++ backend/tags/admin.py
@@ -7,5 +7,4 @@ from tags.models import Tag
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'color', 'slug')
     search_fields = ('name', 'slug')
-    list_editable = ('color', 'slug')
-    list_display_links = ('name',)
+    list_editable = ('name', 'color', 'slug')
