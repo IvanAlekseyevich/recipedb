555005332358612b9c6eec0c68476015ee4d9455
pozarnik <pozarnik@tut.by>
2022-06-11 18:20:08 +0300

    Refactoring

--- backend/users/views.py
+++ backend/users/views.py
@@ -13,10 +13,12 @@ class SubscriptionsApiView(APIView):
 
     def get(self, request):
         user = request.user
-        subscriptions = Subscription.objects.filter(subscriber=user)
-        authors_list = [author.author.id for author in subscriptions]
-        authors = User.objects.filter(id__in=authors_list)
-        serializer = serializers.SubscriptionsSerializer(authors, many=True)
+        authors = User.objects.filter(subscribers__subscriber=user)
+        serializer = serializers.SubscriptionsSerializer(
+            authors,
+            many=True,
+            context={'request': request}
+        )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -38,23 +40,15 @@ class SubscribeApiView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
         else:
-            new_subscribe = Subscription.objects.create(author=author, subscriber=user)
-            new_subscribe.save()
-            serializer = serializers.SubscriptionsSerializer(author)
+            Subscription.objects.create(author=author, subscriber=user)
+            serializer = serializers.SubscriptionsSerializer(author, context={'request': request})
             return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, user_id):
         author = get_object_or_404(User, id=user_id)
         user = request.user
-        if author == user:
-            return Response(
-                {"errors": "Нельзя отписаться от себя!"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
         if Subscription.objects.filter(author=author, subscriber=user).exists():
-            subscribe = Subscription.objects.get(author=author, subscriber=user)
-            subscribe.delete()
+            Subscription.objects.get(author=author, subscriber=user).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(
