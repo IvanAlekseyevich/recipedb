eab51793be9c93c6868f31ab88793a188366bdd3
pozarnik <pozarnik@tut.by>
2022-06-11 19:23:30 +0300

    Change to ReadOnlyModelViewSet

--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -1,18 +1,15 @@
 from rest_framework import filters, viewsets
 
 from ingredients.models import Ingredient
-from ingredients.permissions import IsAdminOrReadOnly
+from ingredients.permissions import ReadOnly
 from ingredients.serializers import IngredientSerializer
 
 
-class IngredientViewSet(viewsets.ModelViewSet):
-    """
-    Возвращает список ингридиентов, либо конкретный ингридиент.
-    Создает/изменяет/удаляет ингридиент. Возможен поиск по названию.
-    """
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """Возвращает список ингредиентов с возможностью поиска по имени."""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [ReadOnly]
     filter_backends = (filters.SearchFilter,)
     pagination_class = None
     search_fields = ('^name',)
--- backend/tags/views.py
+++ backend/tags/views.py
@@ -1,16 +1,13 @@
 from rest_framework import viewsets
 
 from tags.models import Tag
-from tags.permissions import IsAdminOrReadOnly
+from tags.permissions import ReadOnly
 from tags.serializers import TagSerializer
 
 
-class TagViewSet(viewsets.ModelViewSet):
-    """
-    Возвращает список тегов, либо конкретный тег,
-    создает/изменяет/удаляет тег.
-    """
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Возвращает список тегов."""
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [ReadOnly]
     pagination_class = None
