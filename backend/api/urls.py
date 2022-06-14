6f24d04c73c42e89abae8c1d26bc05957937cbb4
pozarnik <pozarnik@tut.by>
2022-06-14 13:50:01 +0300

    Fix endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,17 +8,16 @@ from users.views import SubscribeApiView, SubscriptionsApiView
 app_name = 'api'
 
 router = DefaultRouter()
-router.register('ingredients/', views.IngredientViewSet, basename='ingredients')
-router.register('tags/', views.TagViewSet, basename='tags')
-router.register('recipes/', views.RecipeViewSet, basename='recipes')
-router.register('users/', UserViewSet)
+router.register('ingredients', views.IngredientViewSet, basename='ingredients')
+router.register('tags', views.TagViewSet, basename='tags')
+router.register('recipes', views.RecipeViewSet, basename='recipes')
+router.register('users', UserViewSet)
 
 urlpatterns = [
-
-    path('<int:recipe_id>/shopping_cart/', views.ShoppingCartApiView.as_view(), name='shopping_cart'),
-    path('<int:recipe_id>/favorite/', views.FavoriteRecipeApiView.as_view(), name='favorite_recipe'),
+    path('recipes/<int:recipe_id>/shopping_cart/', views.ShoppingCartApiView.as_view(), name='shopping_cart'),
+    path('recipes/<int:recipe_id>/favorite/', views.FavoriteRecipeApiView.as_view(), name='favorite_recipe'),
     path('recipes/download_shopping_cart/', views.DownloadShopping.as_view(), name='shopping_list'),
-    path('subscriptions/', SubscribeApiView.as_view(), name='subscriptions'),
-    path('<int:user_id>/subscribe/', SubscriptionsApiView.as_view(), name='subscribe'),
+    path('users/subscriptions/', SubscriptionsApiView.as_view(), name='subscriptions'),
+    path('users/<int:user_id>/subscribe/', SubscribeApiView.as_view(), name='subscribe'),
     path('', include(router.urls)),
 ]
