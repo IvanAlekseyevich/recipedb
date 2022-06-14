e49af0d0fed864ab5883177a4d19b52f45f3dae4
pozarnik <pozarnik@tut.by>
2022-06-14 23:45:02 +0300

    Add CurrentUserOrAdminOrReadOnly permission for djoser

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework import permissions
 
 
+class CurrentUserOrAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if type(obj) == type(user) and obj == user:
+            return True
+        return request.method in permissions.SAFE_METHODS or user.is_staff
+
+
 class IsAuthorOrStaffOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
