2925af52dd433940ce6924c6f087018cb20407cb
pozarnik <pozarnik@tut.by>
2022-06-16 23:11:24 +0300

    Add pagination

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users import serializers
@@ -14,13 +15,16 @@ class SubscriptionsApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
+        paginator = PageNumberPagination()
+        paginator.page_size = 6
         authors = User.objects.filter(subscribers__subscriber=request.user)
+        result_page = paginator.paginate_queryset(authors, request)
         serializer = serializers.UserWithRecipesSerializer(
-            authors,
+            result_page,
             many=True,
             context={'request': request}
         )
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return paginator.get_paginated_response(serializer.data)
 
 
 class SubscribeApiView(APIView):
