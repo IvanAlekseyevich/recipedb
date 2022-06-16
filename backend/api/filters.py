23609c20ea9b3770a20214a32d953ea816dceae6
pozarnik <pozarnik@tut.by>
2022-06-16 14:51:48 +0300

    Add tag filter for recipe

---
+++ backend/api/filters.py
@@ -0,0 +1,10 @@
+import django_filters as filters
+from recipes.models import Recipe
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags')
