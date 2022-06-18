1ae04d5cda6908819b9dac14521e9eeedd39eb50
pozarnik <pozarnik@tut.by>
2022-06-18 14:33:10 +0300

    Update swager, redoc endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,29 +19,21 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include('api.urls', namespace='api')),
-]
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Foodgram API",
         default_version='v1',
-        description="Документация для приложения api проекта foodgram",
-        contact=openapi.Contact(email="admin@foodgram.ru"),
+        description="Project documentation",
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
 
-urlpatterns += [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-            name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-            name='schema-redoc'),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/', include('api.urls', namespace='api')),
+    re_path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
