8556ad85af73765d85f2e995d85d3bb9d0c8ed96
pozarnik <pozarnik@tut.by>
2022-06-23 01:28:06 +0300

    Delete list_editable and display id

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -16,18 +16,15 @@ class RecipeTagInline(admin.StackedInline):
 
 @admin.register(models.Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'measurement_unit')
+    list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
-    list_filter = ('name',)
-    list_editable = ('name', 'measurement_unit')
     ordering = ('name',)
 
 
 @admin.register(models.Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'color', 'slug')
+    list_display = ('name', 'color', 'slug')
     search_fields = ('name', 'slug')
-    list_editable = ('name', 'color', 'slug')
     ordering = ('name',)
 
 
@@ -39,34 +36,28 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = (RecipeIngredientInline, RecipeTagInline)
     list_display = ('name', 'author', 'favorite', 'pub_date')
     search_fields = ('author', 'name', 'tags')
-    list_filter = ('author', 'name', 'tags')
-    list_editable = ('name',)
     list_display_links = ('author',)
 
 
 @admin.register(models.RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipe', 'ingredient', 'amount')
+    list_display = ('recipe', 'ingredient', 'amount')
     search_fields = ('recipe', 'ingredient')
-    list_editable = ('ingredient', 'amount')
 
 
 @admin.register(models.RecipeTag)
 class RecipeTagAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipe', 'tag')
+    list_display = ('recipe', 'tag')
     search_fields = ('recipe', 'tag')
-    list_editable = ('tag',)
 
 
 @admin.register(models.FavoriteRecipe)
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'recipe')
+    list_display = ('user', 'recipe')
     search_fields = ('user',)
-    list_editable = ('recipe',)
 
 
 @admin.register(models.ShoppingCart)
 class ShoppingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'recipe')
+    list_display = ('user', 'recipe')
     search_fields = ('user',)
-    list_editable = ('recipe',)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,16 +5,15 @@ from users.models import Subscription, User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_display = ('username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('username', 'email')
-    list_editable = ('email', 'first_name', 'last_name')
     list_display_links = ('username',)
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'subscriber')
+    list_display = ('author', 'subscriber')
     search_fields = ('author', 'subscriber')
 
 
