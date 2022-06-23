459401551623648a2e8e7963facf662a347bbc46
pozarnik <pozarnik@tut.by>
2022-06-23 16:16:28 +0300

    Delete list_filter

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -8,7 +8,6 @@ from users.models import Subscription, User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('username', 'email')
     list_display_links = ('username',)
     fieldsets = (
         (None, {
